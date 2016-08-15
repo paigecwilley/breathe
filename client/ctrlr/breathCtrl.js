01a962506da62c83c685eb3bc925aa591a09dca2
Paige C Willey <paige.catherine.willey@gmail.com>
2016-08-15 17:46:57 -0600

    Removed jquery from breathCtrl

--- client/ctrlr/breathCtrl.js
+++ client/ctrlr/breathCtrl.js
@@ -2,6 +2,10 @@ angular.module('medApp')
 	.controller('breathCtrl', function($scope, $http, $state, $stateParams){
 
 
+
+
+
+
 var inhaleInterval = 0;
 inhaleSecs = [];
 var exhaleExists = false;
@@ -20,15 +24,20 @@ var breathArr = [];
 
 //On keydown of space bar, initiate inhale 
 //Gets time inhale started
-$("body").on("keydown", (function(e){
+
+window.addEventListener("keydown", (function(e){
 	
 if(intBreathArr.length <= 5){
 	var inhaleInitiation;
-	console.log('The keydown works');
+	console.log('keydown listener');
+	// console.log(e.key);
 
-	if(e.keyCode === 32){
+	if(e.key === " "){
 		inhaleInitiation = Date.now();
 		inhaleSecs.push(inhaleInitiation);
+		console.log(e.key);
+		console.log(e.key);
+		console.log("inside the if statement");
 
 	}
 }
@@ -40,16 +49,17 @@ if(intBreathArr.length <= 5){
 
 
 //Initiate beginning of exhale on keyup
-$("body").on("keyup", (function(e){
+window.addEventListener("keyup", (function(e){
 	var intBreathObj = {};
 	inhaleBegin = inhaleSecs[0];
 	inhaleSecs = [];
 	
 	
 	if(intBreathArr.length <= 5){
-		if(e.keyCode === 32){
+		if(e.key === " "){
 		exhaleInitiation = Date.now();
-		// console.log(exhaleInitiation);
+		console.log(exhaleInitiation);
+		console.log('Inside the keyup')
 	}
 
 
@@ -127,9 +137,142 @@ function createBreathCycle(){
 function sendData(data){
 	console.log('Data from breathCtrl: ', data);
 	console.log('State params: ', $stateParams.username);
-	$http.post('http://localhost:3000/user-breath/' + $stateParams.username, data);
-	$state.go('practice', {username:  $stateParams.username})
+	// $http.post('http://localhost:3000/user-breath/' + $stateParams.username, data);
+	// $state.go('practice', {username:  $stateParams.username})
 }
+
+
+///JQUERY VERSION
+// var inhaleInterval = 0;
+// inhaleSecs = [];
+// var exhaleExists = false;
+// var inhaleBegin;
+// var exhaleInitiation;
+
+// // intBreathObj = {};
+
+// var intBreathArr = [];
+
+
+// var breathArr = [];
+
+
+
+
+// //On keydown of space bar, initiate inhale 
+// //Gets time inhale started
+// $("body").on("keydown", (function(e){
+	
+// if(intBreathArr.length <= 5){
+// 	var inhaleInitiation;
+// 	console.log('The keydown works');
+
+
+// 	if(e.code === "Space"){
+// 		inhaleInitiation = Date.now();
+// 		inhaleSecs.push(inhaleInitiation);
+// 		console.log(e.code);
+
+// 	}
+// }
+// // console.log(inhaleSecs);
+
+// }))
+
+
+
+
+// //Initiate beginning of exhale on keyup
+// $("body").on("keyup", (function(e){
+// 	var intBreathObj = {};
+// 	inhaleBegin = inhaleSecs[0];
+// 	inhaleSecs = [];
+	
+	
+// 	if(intBreathArr.length <= 5){
+// 		if(e.code === "Space"){
+// 		exhaleInitiation = Date.now();
+// 		// console.log(exhaleInitiation);
+// 	}
+
+
+// 	// console.log("inhale length " +inhaleLength);
+
+// 	intBreathObj.inhaleTS=inhaleBegin;
+// 	intBreathObj.exhaleTS=exhaleInitiation;
+
+// 	console.log('intBreathObj:', intBreathObj)
+	
+	
+// 	intBreathArr.push(intBreathObj);
+// 	// console.log("Int Arr: ", intBreathArr);
+// 	console.log("Intermediate Breath Array: ", intBreathArr);
+
+// 	if(intBreathArr.length === 6){
+// 		createBreathObjArr();
+// 		sendData(createBreathCycle());
+// 	}
+
+// }
+
+// }))
+
+
+
+	
+// function createBreathObjArr(){
+// for(var i = 0; i < intBreathArr.length; i++){
+// 	var breathObj = {};
+// 			breathObj.inhaleLength = intBreathArr[i].exhaleTS - intBreathArr[i].inhaleTS;
+// 			if(i < 5) {
+// 			breathObj.exhaleLength = intBreathArr[i + 1].inhaleTS - intBreathArr[i].exhaleTS;
+// 			breathObj.totalLength = breathObj.inhaleLength + breathObj.exhaleLength;
+// 		}
+// 			console.log("breath Obj: " , breathObj);
+// 			breathArr.push(breathObj);
+// 			// breathObj = {};
+// 			console.log("breath array: ", breathArr)
+// 	}
+// }
+
+
+
+// //Create Object with average inhale time,
+// //average exhale time,
+// //average total breath time
+
+// var inTotal = 0;
+// var exTotal = 0;
+// var breathTotal = 0;
+
+// function createBreathCycle(){
+// 	var breathCycle = {};
+
+// 		console.log('From the breath cycle');
+// 		for(var i = 0; i < breathArr.length - 1; i++){
+// 			inTotal += breathArr[i].inhaleLength;
+// 			exTotal += breathArr[i].exhaleLength;
+// 			breathTotal += breathArr[i].totalLength;
+// 		}
+
+// 		console.log('Totals: ', inTotal, exTotal, breathTotal);
+
+// 		breathCycle.created = moment().format();
+// 		breathCycle.inhaleAvg = (inTotal/breathArr.length)/1000;
+// 		breathCycle.exhaleAvg = (exTotal/breathArr.length)/1000;
+// 		breathCycle.breathTotalAvg = (breathTotal/breathArr.length)/1000;
+
+// 		return JSON.stringify(breathCycle);
+
+
+// }
+
+// function sendData(data){
+// 	console.log('Data from breathCtrl: ', data);
+// 	console.log('State params: ', $stateParams.username);
+// 	$http.post('http://localhost:3000/user-breath/' + $stateParams.username, data);
+// 	$state.go('practice', {username:  $stateParams.username})
+// }
 
 
 
